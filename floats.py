dfe0a463908ea81d262d93a30b9d774e38ea8e50
amirkle27 <amirkle@gmail.com>
2024-07-04 21:34:51 +0300

    Add files via upload

---
+++ floats.py
@@ -0,0 +1,8 @@
+#start
+x: float = 10.88
+y: float = 65.19
+print (x-y)
+fname : str = input ("Whst's your first name?")
+lname : str = input ("What's your last name?")
+print (fname + " " + lname + ".")
+#End
---
+++ temperature.py
@@ -0,0 +1,5 @@
+temp: float = float (input ("What's the temperature?"))
+if temp>30:
+    print ("Hot Day")
+else:
+    print ("Normal Day")
